f0288be3a2fbc6539e551df9861203d9058dffd6
graaape <default@default.com>
2024-07-10 22:08:09 +0800

    feat: draw a triangle

--- main.cpp
+++ main.cpp
@@ -6,12 +6,13 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
+const TGAColor green =TGAColor(34,139,34,255);
 Model *model = NULL;
 const int width  = 800;
 const int height = 800;
 
 //draw a line from P0(x0,y0) to P1(x1,y1)
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) { 
+void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color) { 
     // hard to decide how many pixels to be drawn
 	/*
 	for (float t=0.; t<1.; t+=.01) { 
@@ -30,6 +31,10 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
         image.set(x, y, color); 
     } 
    */
+   int x0=p0.x;
+   int y0=p0.y;
+   int x1=p1.x;
+   int y1=p1.y;
   bool steep=false;
   if(std::abs(x0-x1)<std::abs(y0-y1)){
 	// swap x y
@@ -74,7 +79,12 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     } 
 	*/
 }
+void triangle(Vec2i p0,Vec2i p1,Vec2i p2,TGAImage &image,TGAColor color){
+	line(p0,p1,image,color);
+	line(p0,p2,image,color);
+	line(p1,p2,image,color);
 
+}
 int main(int argc, char** argv) {
 	TGAImage image(width, height, TGAImage::RGB);
 	// image.set(52, 41, red);
@@ -83,23 +93,30 @@ int main(int argc, char** argv) {
 	// line(80, 40, 13, 20, image, white);
 
 	//draw a head
-	model = new Model("obj/african_head.obj");
-	for (int i=0; i<model->nfaces(); i++) { 
-		std::vector<int> face = model->face(i); 
-		// draw a triangle
-		for (int j=0; j<3; j++) { 
-			Vec3f v0 = model->vert(face[j]); 
-			Vec3f v1 = model->vert(face[(j+1)%3]); 
-			int x0 = (v0.x+1.)*width/2.; 
-			int y0 = (v0.y+1.)*height/2.; 
-			int x1 = (v1.x+1.)*width/2.; 
-			int y1 = (v1.y+1.)*height/2.; 
-			line(x0, y0, x1, y1, image, white); 
-		} 
-	}
+	// model = new Model("obj/african_head.obj");
+	// for (int i=0; i<model->nfaces(); i++) { 
+	// 	std::vector<int> face = model->face(i); 
+	// 	// draw a triangle
+	// 	for (int j=0; j<3; j++) { 
+	// 		Vec3f v0 = model->vert(face[j]); 
+	// 		Vec3f v1 = model->vert(face[(j+1)%3]); 
+	// 		int x0 = (v0.x+1.)*width/2.; 
+	// 		int y0 = (v0.y+1.)*height/2.; 
+	// 		int x1 = (v1.x+1.)*width/2.; 
+	// 		int y1 = (v1.y+1.)*height/2.; 
+	// 		line(x0, y0, x1, y1, image, white); 
+	// 	} 
+	// }
+	Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
+    Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
+    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+
+    triangle(t0[0], t0[1], t0[2], image, red);
+    triangle(t1[0], t1[1], t1[2], image, white);
+    triangle(t2[0], t2[1], t2[2], image, green);
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
-	delete model;
+	// delete model;
 	return 0;
 }
 
